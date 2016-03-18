37e564234195c24bbb8e1adf6ef28abcd8ca53c5
emilkirschner <r0g0jin@r1>
2016-03-18 23:46:57 +0100

    iterative decision tree builder

--- decisiontree/src/test/java/com/thekirschners/ml/decisiontree/TreeTest.java
+++ decisiontree/src/test/java/com/thekirschners/ml/decisiontree/TreeTest.java
@@ -2,9 +2,11 @@ package com.thekirschners.ml.decisiontree;
 
 import com.thekirschners.ml.data.TestDataSet;
 import com.thekirschners.ml.data.Tuple;
+import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.junit.runners.model.TestClass;
 
 import java.util.List;
 
@@ -12,10 +14,12 @@ import java.util.List;
  * Created by emilkirschner on 14/03/16.
  */
 @RunWith(JUnit4.class)
-public class RecParentNodeTest {
+public class TreeTest {
     @Test
     public void testDecisionTree() {
         List<Tuple> dataSet = TestDataSet.getDataSet();
-        TreeNode iterative = TreeNode.Tools.Builder.selector(new InfoGainSplitAttrSelector()).build(dataSet, new Integer[]{1, 2, 3, 4}, 5);
+        TreeNode root = TreeNode.Tools.Builder.selector(new InfoGainSplitAttrSelector()).build(dataSet, new Integer[]{1, 2, 3, 4}, 5);
+        Object predict = TreeNode.Tools.Oracle.predict(root, dataSet.get(0));
+        TestCase.assertEquals(dataSet.get(0).attribute(5), predict);
     }
 }
